2a0fe79eb6a66c36358be3234ec207b025246c23
SijmeJan <s.j.paardekooper@qmul.ac.uk>
2020-09-22 23:52:57 +0100

    third law

--- nbody/plot.py
+++ nbody/plot.py
@@ -117,3 +117,56 @@ class AnimKep2():
         xy = np.transpose(np.asarray([xsel, ysel]))
         self.coll.set_xy(xy)
         return (self.coll,)
+
+
+class AnimKep3():
+    def __init__(self, orb1, orb2):
+        self.xs = np.asarray(orb1.star_pos)[:,0]
+        self.ys = np.asarray(orb1.star_pos)[:,1]
+
+        self.xp1 = np.asarray(orb1.plan_pos)[:,0]
+        self.yp1 = np.asarray(orb1.plan_pos)[:,1]
+
+        #self.xp2 = np.asarray(orb2.plan_pos)[::2,0]
+        #self.yp2 = np.asarray(orb2.plan_pos)[::2,1]
+        self.xp2 = (0.5)**(2/3)*np.asarray(orb1.plan_pos)[::2,0]
+        self.yp2 = (0.5)**(2/3)*np.asarray(orb1.plan_pos)[::2,1]
+        self.xp2 = np.append(self.xp2, self.xp2)
+        self.yp2 = np.append(self.yp2, self.yp2)
+
+    def show(self, fig, ax, plot_p1, plot_p2, plot_s):
+        self.linep1 = plot_p1
+        self.linep2 = plot_p2
+        self.lines = plot_s
+
+        xp1 = np.append(self.xp1, self.xp1[0])
+        yp1 = np.append(self.yp1, self.yp1[0])
+        xp2 = np.append(self.xp2, self.xp2[0])
+        yp2 = np.append(self.yp2, self.yp2[0])
+
+        self.linep1.set_data(xp1, yp1)
+        self.linep2.set_data(xp2, yp2)
+        self.lines.set_data(self.xs[0], self.ys[0])
+
+        ax.relim()
+        ax.autoscale()
+
+        self.anim = animation.FuncAnimation(fig, self.animate,
+                                            init_func = self.init,
+                                            frames = len(self.xp1),
+                                            repeat=True,
+                                            interval=20, blit=True)
+
+        return HTML(self.anim.to_jshtml())
+
+    def init(self):
+        self.linep1.set_data([self.xp1[0]],[self.yp1[0]])
+        self.linep2.set_data([self.xp2[0]],[self.yp2[0]])
+        self.lines.set_data([self.xs[0]],[self.ys[0]])
+        return (self.linep2,)
+
+    def animate(self, i):
+        self.linep1.set_data([self.xp1[i]],[self.yp1[i]])
+        self.linep2.set_data([self.xp2[i]],[self.yp2[i]])
+        self.lines.set_data([self.xs[i]],[self.ys[i]])
+        return (self.linep2,)
